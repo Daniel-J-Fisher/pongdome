6bbd8b6cf79ce461a1d2b257015da68e32c30018
Daniel Fisher <dfishe33@gmail.com>
2018-03-06 10:18:39 -0500

    Working with server

--- web/index.js
+++ web/index.js
@@ -5,10 +5,4 @@ app.get('/', (req, res) => {
     res.send('index.html');
 });
 
-exports.run = (config) => {
-    app.listen(80, ()=>console.log('Webserver listening on port 80'));
-}
-
-if (require.main === module) {
-    exports.run()
-}
+app.listen(80, ()=>console.log('Webserver listening on port 80'));
