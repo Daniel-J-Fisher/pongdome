11f2b1b66e02ce0b4bf9709b8be14ae073f08648
Daniel Fisher <dfishe33@gmail.com>
2018-03-06 10:35:19 -0500

    Removed line causing error

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const api = require('./api')
 const chat = require('./chat')
 const gpio = process.platform === 'linux' ? require('./gpio') : { run () {} }
 const screen = require('./screen')
-const web = require('./web');
+const web = require('./web')
 
 api.run()
 chat.run()
 gpio.run()
 screen.run()
-web.run();
