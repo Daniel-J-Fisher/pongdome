3bba74bb5da716484b574981c38427e2b618442c
Daniel Fisher <dfishe33@gmail.com>
2018-03-06 11:05:25 -0500

    Added static file serve

--- web/index.js
+++ web/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.static('css'));
+app.use(express.static('img'));
+app.use(express.static('js'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
