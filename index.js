03b2b782c1f12716cb48780a9ea3418e63f0c0d1
Daniel Fisher <dfishe33@gmail.com>
2018-03-06 09:49:56 -0500

    Added express webserver to host the index.html file

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const api = require('./api')
 const chat = require('./chat')
 const gpio = process.platform === 'linux' ? require('./gpio') : { run () {} }
 const screen = require('./screen')
+const web = require('./web');
 
 api.run()
 chat.run()
 gpio.run()
 screen.run()
+web.run();
