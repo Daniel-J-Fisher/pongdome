f9cf48ad7451e94da9a20e26cc2e70e0662d4eae
Daniel Fisher <dfishe33@gmail.com>
2018-03-06 11:11:29 -0500

    Fixed static serve

--- web/index.js
+++ web/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('css'));
-app.use(express.static('img'));
-app.use(express.static('js'));
+app.use('/css', express.static('css'));
+app.use('/img', express.static('img'));
+app.use('/js', express.static('js'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
