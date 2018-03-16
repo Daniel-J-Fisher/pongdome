0c916783ea3228519d57528af0582614c7e3ea60
Daniel Fisher <dfishe33@gmail.com>
2018-03-16 17:01:35 -0400

    Moved files again

--- web/index.js
+++ web/index.js
@@ -3,7 +3,13 @@ const app = express();
 const path = require('path');
 const debug = require('../debug')('pongdome:web')
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/img', express.static(path.join(__dirname, 'img')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
 
 exports.run = (config)=>{
     app.listen(8080, () => debug('PongDome Webserver listening on port 8080'));
